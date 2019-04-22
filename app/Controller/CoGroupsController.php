7ce458cbbc599c061ea78cd7923bafe7e18318a8
nikosev <nikos.ev@hotmail.com>
2019-04-22 14:17:32 +0300

    Fix version of search function on Groups

--- app/Controller/CoGroupsController.php
+++ app/Controller/CoGroupsController.php
@@ -716,7 +716,7 @@ class CoGroupsController extends StandardController {
    * Insert search parameters into URL for index.
    * - postcondition: Redirect generated
    *
-   * @since  COmanage Registry v0.8
+   * @since  COmanage Registry RCIAM v3.1.1
    */
   
   public function search() {
