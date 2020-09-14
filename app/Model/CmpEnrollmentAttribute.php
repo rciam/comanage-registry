525d5dde380ec4e0d6213d0514e9bdf870c653ee
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2020-09-14 18:59:13 +0300

    Remove VO Model and all its dependencies from COmanage core.

--- app/Model/CmpEnrollmentAttribute.php
+++ app/Model/CmpEnrollmentAttribute.php
@@ -231,14 +231,6 @@ class CmpEnrollmentAttribute extends AppModel {
         'ldap_name' => '',
         'saml_name' => 'distinguishedName'
       ),
-      'vos:vo_targets_id' => array(
-        'type'      => VOsEnum::VO,
-        'label'     => _txt('fd.vos.vo_name') . " (" . _txt('en.vos.type', null, VOsEnum::VO) . ")",
-        'required'  => RequiredEnum::Optional,
-        'env_name'  => 'CMP_EF_VONAME',
-        'ldap_name' => '',
-        'saml_name' => ''
-      )
     );
     
     return $attributes;
